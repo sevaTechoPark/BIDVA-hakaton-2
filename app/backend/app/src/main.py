15f89dc5b537d135af0cc5c6a82093ea1c525540
Seva <v.papadyk@vk.team>
2025-12-23 00:42:47 +0300

    fix CORS error

--- app/backend/app/src/main.py
+++ app/backend/app/src/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import main_router
 
 app = FastAPI(title="RAG Model")
 app.include_router(main_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # или ["*"] для всех, но лучше явно
+    allow_credentials=True,
+    allow_methods=["*"],  # или перечисли нужные методы: ["GET", "POST", ...]
+    allow_headers=["*"],  # или перечисли нужные заголовки
+)
